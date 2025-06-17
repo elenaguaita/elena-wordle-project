305d52d3e1ecd4e8d5a1ffc9f98e82222830957f
elenaguaita <e.elenaguaita@gmail.com>
2025-06-17 12:13:06 +0200

    Add MUI support

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,13 @@
 import React, { JSX } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App(): JSX.Element {
-  return <div className="wrapper"></div>;
+  return (
+    <>
+      <CssBaseline />
+      <div className="wrapper"></div>;
+    </>
+  );
 }
 
 export default App;
