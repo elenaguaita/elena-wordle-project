9d6d412a62b1cfd22d36b7495c6a971c86eeef92
elenaguaita <e.elenaguaita@gmail.com>
2025-06-17 11:12:42 +0200

    Add index, basic style and App component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import React, { JSX } from "react";
+
+function App(): JSX.Element {
+  return <div className="wrapper"></div>;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./components/App/App";
+
+import "./styles.css";
+
+const container = document.querySelector("#root");
+if (!container) {
+  throw new Error("Root container missing in index.html");
+}
+
+const root = createRoot(container);
+root.render(<App />);
